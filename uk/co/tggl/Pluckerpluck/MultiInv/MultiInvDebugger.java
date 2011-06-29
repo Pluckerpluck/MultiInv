4a2f5e2e1271d988643182a4e0ea988e027745ff
Sammy Gillespie <sammygillespie@gmail.com>
2011-06-29 16:04:32 +0100

    Setting up new repo - Plugin v2.3.0

---
+++ uk/co/tggl/Pluckerpluck/MultiInv/MultiInvDebugger.java
@@ -0,0 +1,206 @@
+package uk.co.tggl.Pluckerpluck.MultiInv;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import org.bukkit.entity.Player;
+
+import uk.co.tggl.Pluckerpluck.MultiInv.MultiInvEnums.MultiInvEvent;
+
+public class MultiInvDebugger {
+	
+public final MultiInv plugin;
+
+	private boolean debugging = false;
+	private ArrayList<String> debuggers = new ArrayList<String>();
+	private String dividerStart = "#-----";
+	private String dividerEnd = "-----#";
+	private ArrayList<String> logHistory = new ArrayList<String>();
+	private boolean debugLogging = true;
+   
+    public MultiInvDebugger(MultiInv instance) {
+        plugin = instance;
+    }
+    
+    public void addDebugger (Player player){
+    	if (!(debuggers.contains(player.getName()))){
+    		debuggers.add(player.getName());
+    		debugging=true;
+    	}
+    }
+    
+    public void removeDebugger (Player player){
+    	if (debuggers.contains(player.getName())){
+    		debuggers.remove(player.getName());
+    	}
+    }
+    
+    public void startDebugging (){
+    	debugging = true;
+    }
+    
+    public void stopDebugging (){
+    	saveDebugLog();
+    	debuggers.clear();
+    	debugging = false;
+    }
+    public void saveDebugLog(){
+    	if (debugLogging && debugging){
+    		logToFile();
+    		logHistory.clear();
+    	}
+    }
+    
+    
+    public void debugEvent(MultiInvEvent event, String[] args){
+    	String message = "";
+    	String message2 = "";
+    	if (debugging == true){
+    		switch (event){
+    			case WORLD_CHANGE:
+    				message = dividerStart + args[0] + " changed world" + dividerEnd;
+    				int shareNumber = shareCheck(args[1], args[2]);
+    				message2 = "";
+    				switch (shareNumber){
+    					case 0:
+    						message2 = "Moved from " + args[1] + " to " + args[2];
+    						break;
+    					case 1:
+    						message2 = "Moved from " + args[1] + "* to " + args[2];
+    						break;
+    					case 2:
+    						message2 = "Moved from " + args[1] + " to " + args[2] + "*";
+    						break;
+    					case 3:
+    						message2 = "Moved from " + args[1] + "* to " + args[2] + "*";
+    						break;
+    					case 4:
+    						message2 = "Moved from " + args[1] + " to " + args[2] + " (Shared)";
+    						break;
+    					default:
+    						message2 = "Error with WORLD_CHANAGE debug event";
+    						break;
+    				}
+    				sendDebuggersMessage(message);
+    				sendDebuggersMessage(message2);
+    				break;
+    			case INVENTORY_SAVE:
+    				message  = "Saved '" + args[0] + "'";
+    				sendDebuggersMessage(message);
+    				break;
+    			case INVENTORY_LOAD:
+    				message  = "Loaded '" + args[0] + "'";
+    				sendDebuggersMessage(message);
+    				break;
+    			case INVENTORY_NEW:
+    				message  = "Creating new inventory for '" + args[0] + "'";
+    				sendDebuggersMessage(message);
+    				break;
+    			case FILE_SAVE:
+    				message  = "Saved inventories to file";
+    				sendDebuggersMessage(message);
+    				break;
+    			case FILE_LOAD:
+    				message  = "Loaded inventories from file";
+    				sendDebuggersMessage(message);
+    				break;
+    			case PLAYERS_UPDATE:
+    				message  = "Players list updated";
+    				sendDebuggersMessage(message);
+    				break;
+    			case INVENTORY_DELETE:
+    				message  = "'" + args[0] + "' has been deleted";
+    				sendDebuggersMessage(message);
+    				break;
+    			case INVENTORY_ADDED:
+    				message  = "'" + args[0] + "' has been added";
+    				sendDebuggersMessage(message);
+    				break;
+    			case PLAYER_LOGIN:
+    				message = dividerStart + args[0] + " logged in" + dividerEnd;
+    				sendDebuggersMessage(message);
+    				break;
+    			case PLAYER_LOGOUT:
+    				message = dividerStart + args[0] + " logged out" + dividerEnd;
+    				sendDebuggersMessage(message);
+    				break;
+    			case INVENTORY_LOAD_NULL:
+    				message  = "Loaded empty inventory for '" + args[0] + "'";
+    				sendDebuggersMessage(message);
+    				break;
+    			case INVENTORY_LOAD_NEW:
+    				message  = "Loaded new inventory for '" + args[0] + "'";
+    				sendDebuggersMessage(message);
+    				break;
+    			case INVENTORY_DELETE_UNUSED:
+    				message  = "'" + args[0] + "' has been deleted as it is unused";
+    				sendDebuggersMessage(message);
+    				break;
+    			default:
+    				message2 = "Error with "+ event.toString() + " debug event";
+    				sendDebuggersMessage(message);
+    				break;
+    				
+    		}
+    	}
+    }
+    
+    private void sendDebuggersMessage(String message){
+    	if (!(debuggers.isEmpty())){
+	    	for (String player : debuggers){
+	    		Player player2 = plugin.getServer().getPlayer(player);
+	    		if (player2 != null){
+	    			player2.sendMessage(message);
+	    		}
+	    	}
+    	}
+    	if (debugLogging){
+    		logHistory.add(message);
+    	}
+    }
+    
+    private int shareCheck(String world1, String world2){
+    	if (plugin.sharesMap.containsKey(world1)){
+    		if (plugin.sharesMap.containsKey(world2)){
+    			return 3;
+    		}
+			return 1;
+		}
+    	if (plugin.sharesMap.containsKey(world2)){
+			return 2;
+		}
+    	return 0;
+    }
+    
+    private void logToFile() {
+        File file = new File("plugins" + File.separator + "MultiInv" + File.separator + "logs"+ File.separator + "debugLog.txt");
+        String parent = file.getParent();
+        File dir = new File(parent);
+        if (!dir.exists()){
+            dir.mkdir();
+        }
+        int i=1;
+        while(file.exists()){
+        	file = new File("plugins" + File.separator + "MultiInv" + File.separator + "logs"+ File.separator + "debugLog"+i+".txt");
+        	i++;
+        }
+        if (!(logHistory.isEmpty())){
+        //Print to file
+        	try{
+            // Create file 
+	            FileWriter fstream = new FileWriter(file);
+	            BufferedWriter out = new BufferedWriter(fstream);
+	            for (String line : logHistory){
+	            	if (line != null){
+	            		out.write(line + System.getProperty("line.separator"));
+	            	}
+	            }
+	            //Close the output stream
+	            out.close();
+	            }catch (Exception e){//Catch exception if any
+	              System.err.println("MultiInv Error: " + e.getMessage());
+	            }
+        	}
+    }
+}
---
+++ uk/co/tggl/Pluckerpluck/MultiInv/MultiInvProperties.java
@@ -0,0 +1,138 @@
+package uk.co.tggl.Pluckerpluck.MultiInv;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.Set;
+
+public class MultiInvProperties {
+	
+	public static String loadFromProperties(String file, String key){	
+		File FileP = new File(file);
+		return loadFromProperties(FileP, key, null);
+	} 
+	
+	public static String loadFromProperties(String file, String key, String defaultValue){
+		File FileP = new File(file);
+		return loadFromProperties(FileP, key, defaultValue);
+	}
+	
+	
+	public static String loadFromProperties(File file, String key){	
+		return loadFromProperties(file, key, null);
+	} 
+	
+	public static String loadFromProperties(File file, String key, String defaultValue){
+		Properties prop = new Properties();
+		String value = defaultValue;
+    	File dir = new File(file.getParent());
+    	if (!dir.exists()){
+            dir.mkdirs();
+        }
+        if(!file.exists()){
+            try {
+            	file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    	try {
+            FileInputStream in = new FileInputStream(file);
+            prop.load(in);
+            if (prop.containsKey(key)) {
+            	value = prop.getProperty(key);
+            	in.close();
+            }
+        } catch (Exception ex) {
+        	ex.printStackTrace();
+        }
+		return value;
+    }
+	 
+	 public static Set<String> getAllKeys(String file){
+		 File FileP = new File(file);
+		 return getAllKeys(FileP);
+	 }
+	 
+	 public static Set<String> getAllKeys(File file){
+		 Properties prop = new Properties();
+		 Set<String> set = null;
+		 File dir = new File(file.getParent());
+		 if (!dir.exists()){
+			 dir.mkdirs();
+		 }
+		 if(!file.exists()){
+			 try {
+				 file.createNewFile();
+			 } catch (IOException e) {
+				 e.printStackTrace();
+			 }
+		 }
+		 try {
+			 FileInputStream in = new FileInputStream(file);
+			 prop.load(in);
+			 set = prop.stringPropertyNames();
+			 in.close();
+		 } catch (Exception ex) {
+			 ex.printStackTrace();
+		 }
+		 return set;
+	 }
+	 
+	public static void saveToProperties(String file, String key, String value){
+		saveToProperties(file, key, value, "No Comment");
+	}
+	
+	public static void saveToProperties(String file, String key, String value, String comment){
+    	File FileP = new File(file);
+    	Properties prop = new Properties();
+    	File dir = new File(FileP.getParent());
+    	if (!dir.exists()){
+            dir.mkdirs();
+        }
+        if(!FileP.exists()){
+            try {
+            	FileP.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    	try {
+            FileInputStream in = new FileInputStream(FileP);
+            prop.load(in);
+            prop.put(key, value);
+            prop.store(new FileOutputStream(FileP), comment);
+            in.close();
+        } catch (Exception ex) {
+        	ex.printStackTrace();
+        }
+    }
+	
+	public static void removeProperty(String file, String key, String comment){
+    	File FileP = new File(file);
+    	Properties prop = new Properties();
+    	File dir = new File(FileP.getParent());
+    	if (!dir.exists()){
+            dir.mkdirs();
+        }
+        if(!FileP.exists()){
+            try {
+            	FileP.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    	try {
+            FileInputStream in = new FileInputStream(FileP);
+            prop.load(in);
+            prop.remove(key);
+            prop.store(new FileOutputStream(FileP), comment);
+            in.close();
+        } catch (Exception ex) {
+        	ex.printStackTrace();
+        }
+    }
+}
+
