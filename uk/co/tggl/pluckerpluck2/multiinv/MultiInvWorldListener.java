14871a36052ac5fa7c153dbe33d9cb387ed21d87
Sammy Gillespie <sammygillespie@gmail.com>
2011-08-18 19:15:28 +0100

    Part 2

---
+++ uk/co/tggl/pluckerpluck2/multiinv/MultiInvWorldListener.java
@@ -0,0 +1,21 @@
+package uk.co.tggl.pluckerpluck.multiinv;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.world.WorldListener;
+import org.bukkit.event.world.WorldSaveEvent;
+
+public class MultiInvWorldListener extends WorldListener {
+
+    public final MultiInv plugin;
+
+    public MultiInvWorldListener(MultiInv instance) {
+        plugin = instance;
+    }
+
+    @Override
+    public void onWorldSave(WorldSaveEvent event) {
+        for (Player player : plugin.getServer().getOnlinePlayers()) {
+            plugin.playerInventory.storeCurrentInventory(player, player.getWorld().getName());
+        }
+    }
+}
