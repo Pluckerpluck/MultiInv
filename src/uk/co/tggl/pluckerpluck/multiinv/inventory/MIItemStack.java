f954a2e0f362cea76593ee2972ed4f9ac9981ef2
Joshua Reetz <thetux2@gmail.com>
2013-01-24 01:39:42 -0700

    Serialized meta data safer. Added fireworks and charges, plus enchanted books.

--- src/uk/co/tggl/pluckerpluck/multiinv/inventory/MIItemStack.java
+++ src/uk/co/tggl/pluckerpluck/multiinv/inventory/MIItemStack.java
@@ -2,9 +2,15 @@ package uk.co.tggl.pluckerpluck.multiinv.inventory;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.FireworkEffect.Builder;
+import org.bukkit.FireworkEffect.Type;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
+import org.bukkit.inventory.meta.FireworkEffectMeta;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.inventory.meta.PotionMeta;
@@ -21,6 +27,8 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 /**
  * Created by IntelliJ IDEA.
@@ -143,50 +151,187 @@ public class MIItemStack {
     private ItemStack addItemMeta(ItemStack is, String meta) {
         ItemMeta ismeta = is.getItemMeta();
         String[] msplit = meta.split("#");
-        int repairableindex = 2;
-        if(msplit.length > 1) {
-            if(!msplit[0].equals("")) {
-                ismeta.setDisplayName(base64Decode(msplit[0]));
+        //Let's see if we've got new meta or the old way of handling it.
+        if(msplit[0].equals("NM")) {
+            HashMap<String, String> itemdata = new HashMap<String,String>();
+            int potionint = 0;
+            int effectint = 0;
+            int fireworkint = 0;
+            for(String data : msplit) {
+                if(data.equals("NM")) continue;
+                if(data.length() > 1) {
+                    String key = data.substring(0, 1);
+                    //There can be multiple potion effects, let's add an identifier
+                    if(key.equals("P")) {
+                        key = "P" + String.valueOf(potionint);
+                        potionint++;
+                    }else if(key.equals("E")) {
+                        key = "E" + String.valueOf(effectint);
+                        effectint++;
+                    }else if(key.equals("F")) {
+                        key = "F" + String.valueOf(fireworkint);
+                        fireworkint++;
+                    }
+                    String value = data.substring(1);
+                    itemdata.put(key, value);
+                }
             }
-            if(!msplit[1].equals("")) {
-                ismeta.setLore(decodeLore(msplit[1]));
+            String data = itemdata.get("N");
+            if(data != null) {
+                ismeta.setDisplayName(base64Decode(data));
+            }
+            data = itemdata.get("L");
+            if(data != null) {
+                ismeta.setLore(decodeLore(data));
             }
             if(ismeta instanceof SkullMeta) {
-                if(!msplit[2].isEmpty()) {
-                    ((SkullMeta) ismeta).setOwner(msplit[2]);
+                data = itemdata.get("O");
+                if(data != null) {
+                    ((SkullMeta) ismeta).setOwner(data);
                 }
-                repairableindex = 3;
             } else if(ismeta instanceof LeatherArmorMeta) {
-                if(!msplit[2].equals("")) {
-                    int color = Integer.parseInt(msplit[2]);
+                data = itemdata.get("C");
+                if(data != null) {
+                    int color = Integer.parseInt(data);
                     ((LeatherArmorMeta) ismeta).setColor(Color.fromRGB(color));
                 }
-                repairableindex = 3;
             } else if(ismeta instanceof PotionMeta) {
-                if(msplit.length > repairableindex) {
-                    boolean ispotion = true;
-                    if(msplit[repairableindex].contains("+")) {
-                        PotionMeta pmeta = (PotionMeta) ismeta;
-                        for(; repairableindex < msplit.length && ispotion; repairableindex++) {
-                            if(msplit[repairableindex].contains("+")) {
-                                String[] potion = msplit[repairableindex].split("\\+");
-                                PotionEffectType type = PotionEffectType.getByName(potion[0]);
-                                int amplifier = Integer.parseInt(potion[1]);
-                                int duration = Integer.parseInt(potion[2]);
-                                PotionEffect pe = new PotionEffect(type, duration, amplifier);
-                                pmeta.addCustomEffect(pe, true);
-                            } else {
-                                ispotion = false;
-                                repairableindex--;
+                PotionMeta pmeta = (PotionMeta) ismeta;
+                for(int i = 0; (data = itemdata.get("P" + String.valueOf(i))) != null; i++) {
+                    String[] potion = data.split("\\+");
+                    PotionEffectType type = PotionEffectType.getByName(potion[0]);
+                    int amplifier = Integer.parseInt(potion[1]);
+                    int duration = Integer.parseInt(potion[2]);
+                    PotionEffect pe = new PotionEffect(type, duration, amplifier);
+                    pmeta.addCustomEffect(pe, true);
+                }
+            }else if(ismeta instanceof EnchantmentStorageMeta) {
+                EnchantmentStorageMeta emeta = (EnchantmentStorageMeta) ismeta;
+                for(int i = 0; (data = itemdata.get("E" + String.valueOf(i))) != null; i++) {
+                    String[] enchant = data.split("\\+");
+                    Enchantment enchantenum = Enchantment.getByName(enchant[0]);
+                    if(enchantenum != null) {
+                        int value = Integer.parseInt(enchant[1]);
+                        emeta.addStoredEnchant(enchantenum, value, true);
+                    }
+                }
+            }else if(ismeta instanceof FireworkMeta) {
+                FireworkMeta fmmeta = (FireworkMeta) ismeta;
+                for(int i = 0; (data = itemdata.get("F" + String.valueOf(i))) != null; i++) {
+                    String[] fedata = data.split("\\+");
+                    Type effect = FireworkEffect.Type.valueOf(fedata[0]);
+                    Builder ef = FireworkEffect.builder();
+                    ef.with(effect);
+                    if(effect != null) {
+                        String[] colors = fedata[1].split("-");
+                        for(String color : colors) {
+                            try {
+                                ef.withColor(Color.fromRGB(Integer.parseInt(color)));
+                            }catch (Exception e) {
+                                
+                            }
+                        }
+                        String[] fadecolors = fedata[2].split("-");
+                        for(String fadecolor : fadecolors) {
+                            try {
+                                ef.withFade(Color.fromRGB(Integer.parseInt(fadecolor)));
+                            }catch (Exception e) {
+                                
+                            }
+                        }
+                        ef.flicker(Boolean.parseBoolean(fedata[3]));
+                        ef.trail(Boolean.parseBoolean(fedata[4]));
+                        fmmeta.addEffect(ef.build());
+                    }
+                }
+                data = itemdata.get("G");
+                try {
+                    fmmeta.setPower(Integer.parseInt(data));
+                }catch (Exception e) {
+                    
+                }
+            }else if(ismeta instanceof FireworkEffectMeta) {
+                data = itemdata.get("F0");
+                String[] fedata = data.split("\\+");
+                Type effect = FireworkEffect.Type.valueOf(fedata[0]);
+                Builder ef = FireworkEffect.builder();
+                ef.with(effect);
+                if(effect != null) {
+                    String[] colors = fedata[1].split("-");
+                    for(String color : colors) {
+                        try {
+                            ef.withColor(Color.fromRGB(Integer.parseInt(color)));
+                        }catch (Exception e) {
+                            
+                        }
+                    }
+                    String[] fadecolors = fedata[2].split("-");
+                    for(String fadecolor : fadecolors) {
+                        try {
+                            ef.withFade(Color.fromRGB(Integer.parseInt(fadecolor)));
+                        }catch (Exception e) {
+                            
+                        }
+                    }
+                    ef.flicker(Boolean.parseBoolean(fedata[3]));
+                    ef.trail(Boolean.parseBoolean(fedata[4]));
+                    ((FireworkEffectMeta) ismeta).setEffect(ef.build());
+                }
+            }
+            if(ismeta instanceof Repairable) {
+                data = itemdata.get("R");
+                if(data != null) {
+                    Repairable rmeta = (Repairable) ismeta;
+                    rmeta.setRepairCost(Integer.parseInt(data));
+                }
+            }
+        }else {
+            //Old item meta here
+            int repairableindex = 2;
+            if(msplit.length > 1) {
+                if(!msplit[0].equals("")) {
+                    ismeta.setDisplayName(base64Decode(msplit[0]));
+                }
+                if(!msplit[1].equals("")) {
+                    ismeta.setLore(decodeLore(msplit[1]));
+                }
+                if(ismeta instanceof SkullMeta) {
+                    if(!msplit[2].isEmpty()) {
+                        ((SkullMeta) ismeta).setOwner(msplit[2]);
+                    }
+                    repairableindex = 3;
+                } else if(ismeta instanceof LeatherArmorMeta) {
+                    if(!msplit[2].equals("")) {
+                        int color = Integer.parseInt(msplit[2]);
+                        ((LeatherArmorMeta) ismeta).setColor(Color.fromRGB(color));
+                    }
+                    repairableindex = 3;
+                } else if(ismeta instanceof PotionMeta) {
+                    if(msplit.length > repairableindex) {
+                        boolean ispotion = true;
+                        if(msplit[repairableindex].contains("+")) {
+                            PotionMeta pmeta = (PotionMeta) ismeta;
+                            for(; repairableindex < msplit.length && ispotion; repairableindex++) {
+                                if(msplit[repairableindex].contains("+")) {
+                                    String[] potion = msplit[repairableindex].split("\\+");
+                                    PotionEffectType type = PotionEffectType.getByName(potion[0]);
+                                    int amplifier = Integer.parseInt(potion[1]);
+                                    int duration = Integer.parseInt(potion[2]);
+                                    PotionEffect pe = new PotionEffect(type, duration, amplifier);
+                                    pmeta.addCustomEffect(pe, true);
+                                } else {
+                                    ispotion = false;
+                                    repairableindex--;
+                                }
                             }
                         }
                     }
                 }
-            }
-            if(ismeta instanceof Repairable) {
-                if(msplit.length > repairableindex) {
-                    Repairable rmeta = (Repairable) ismeta;
-                    rmeta.setRepairCost(Integer.parseInt(msplit[repairableindex]));
+                if(ismeta instanceof Repairable) {
+                    if(msplit.length > repairableindex) {
+                        Repairable rmeta = (Repairable) ismeta;
+                        rmeta.setRepairCost(Integer.parseInt(msplit[repairableindex]));
+                    }
                 }
             }
         }
@@ -196,27 +341,80 @@ public class MIItemStack {
     
     private String getItemMetaSerialized(ItemMeta meta) {
         StringBuilder smeta = new StringBuilder();
-        smeta.append(base64Encode(meta.getDisplayName()) + "#");
-        smeta.append(encodeLore(meta.getLore()) + "#");
+        smeta.append("NM#");
+        smeta.append("N" + base64Encode(meta.getDisplayName()) + "#");
+        smeta.append("L" + encodeLore(meta.getLore()) + "#");
         if(meta instanceof SkullMeta) {
             SkullMeta skullmeta = (SkullMeta) meta;
             if(((SkullMeta) meta).hasOwner()) {
-                smeta.append(skullmeta.getOwner() + "#");
+                smeta.append("O" + skullmeta.getOwner() + "#");
             }
         } else if(meta instanceof LeatherArmorMeta) {
             Color color = ((LeatherArmorMeta) meta).getColor();
-            smeta.append(String.valueOf(color.asRGB()) + "#");
+            smeta.append("C" + String.valueOf(color.asRGB()) + "#");
         } else if(meta instanceof PotionMeta) {
             if(((PotionMeta) meta).hasCustomEffects()) {
                 List<PotionEffect> effects = ((PotionMeta) meta).getCustomEffects();
                 for(PotionEffect effect : effects) {
-                    smeta.append(effect.getType().getName() + "+" + effect.getAmplifier() + "+" + effect.getDuration() + "#");
+                    smeta.append("P" + effect.getType().getName() + "+" + effect.getAmplifier() + "+" + effect.getDuration() + "#");
                 }
             }
+        }else if(meta instanceof EnchantmentStorageMeta) {
+            EnchantmentStorageMeta emeta = (EnchantmentStorageMeta) meta;
+            if(emeta.hasStoredEnchants()) {
+                Set<Entry<Enchantment,Integer>> enchants = emeta.getStoredEnchants().entrySet();
+                for(Entry<Enchantment,Integer> enchant : enchants) {
+                    smeta.append("E" + enchant.getKey().getName() + "+" + enchant.getValue().toString() + "#");
+                }
+            }
+        }else if(meta instanceof FireworkMeta) {
+            List<FireworkEffect> effects = ((FireworkMeta) meta).getEffects();
+            ((FireworkMeta) meta).getPower();
+            for(FireworkEffect effect : effects) {
+                List<Color> colors = effect.getColors();
+                List<Color> fadecolors = effect.getFadeColors();
+                StringBuilder colorstring = new StringBuilder();
+                for(Color color : colors) {
+                    if(colorstring.length() > 0) {
+                        colorstring.append("-");
+                    }
+                    colorstring.append(Integer.toString(color.asRGB()));
+                }
+                StringBuilder fadecolorstring = new StringBuilder();
+                for(Color color : fadecolors) {
+                    if(fadecolorstring.length() > 0) {
+                        fadecolorstring.append("-");
+                    }
+                    fadecolorstring.append(Integer.toString(color.asRGB()));
+                }
+                smeta.append("F" + effect.getType().name() + "+" + colorstring.toString() + "+" + fadecolorstring.toString() + "+" + 
+                        effect.hasFlicker() + "+" + effect.hasTrail() + "#");
+            }
+            smeta.append("G" + Integer.toString(((FireworkMeta) meta).getPower()) + "#");
+        }else if(meta instanceof FireworkEffectMeta) {
+            FireworkEffect effect = ((FireworkEffectMeta) meta).getEffect();
+            List<Color> colors = effect.getColors();
+            List<Color> fadecolors = effect.getFadeColors();
+            StringBuilder colorstring = new StringBuilder();
+            for(Color color : colors) {
+                if(colorstring.length() > 0) {
+                    colorstring.append("-");
+                }
+                colorstring.append(Integer.toString(color.asRGB()));
+            }
+            StringBuilder fadecolorstring = new StringBuilder();
+            for(Color color : fadecolors) {
+                if(fadecolorstring.length() > 0) {
+                    fadecolorstring.append("-");
+                }
+                fadecolorstring.append(Integer.toString(color.asRGB()));
+            }
+            smeta.append("F" + effect.getType().name() + "+" + colorstring.toString() + "+" + fadecolorstring.toString() + "+" + 
+                    effect.hasFlicker() + "+" + effect.hasTrail() + "#");
         }
         if(meta instanceof Repairable) {
             Repairable rmeta = (Repairable) meta;
-            smeta.append(rmeta.getRepairCost());
+            smeta.append("R" + rmeta.getRepairCost());
         }
         return smeta.toString();
     }
